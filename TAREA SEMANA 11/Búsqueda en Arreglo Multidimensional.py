8143eb4ed5445a76412d0eabe00606ac001a9b03
Cristian Meneses <25crismen@gmail.com>
2024-09-05 11:36:09 -0500

    VERSION 1

---
+++ TAREA SEMANA 11/Búsqueda en Arreglo Multidimensional.py
@@ -0,0 +1,22 @@
+#Definimos elementos de la matriz
+matriz=[
+    [1,2,3],
+    [4,9,6],
+    [7,8,5]
+]
+#Se pide al usuario ingresar el valor que desea buscar
+elemento_a_buscar= int(input("Ingrese el Valor a buscar dentro de la matriz, debe ser un número entero: "))
+# Buscar el elemento en la lista 3x3
+encontrado = False  # verifica si el elemento se encuentra
+for fila in range(len(matriz)):
+    for columna in range(len(matriz[fila])):
+        #Si el elemento se encuentra dentro de la matriz
+        if matriz[fila][columna] == elemento_a_buscar:
+            print(f"El elemento {elemento_a_buscar} se encuentra en el índice [{fila}][{columna}]")
+            encontrado = True
+            break
+# Si no se encuentra el elemento
+if encontrado==False:
+    print(f"El elemento {elemento_a_buscar} no se encuentra en la lista.")
+
+
---
+++ TAREA SEMANA 11/Ordenación de Arreglo Multidimensional.py
@@ -0,0 +1,33 @@
+# Función para ordenar una fila de la matriz usando la función sorted()
+def ordenar_fila(matriz, indice_fila):
+    # Ordenamos la fila usando la función sorted() y la reemplazamos en la matriz
+    matriz[indice_fila] = sorted(matriz[indice_fila])
+
+# Función para imprimir la matriz
+def imprimir_matriz(matriz):
+    for fila in matriz:
+        print(fila)
+
+# Matriz 3x3 inicial
+matriz = [
+    [34, 8, 50],  # Primera fila
+    [12, 25, 18], # Segunda fila
+    [22, 3, 15]   # Tercera fila
+]
+
+#Se ingresa el numero de fila que se desea ordenar
+indice=int(input("Ingrese el número de la fila que desea ordenar: "))
+#Le restamos uno al dato ingresado por el usuario
+#El usuario no sabe que los indices comienzan desde cero
+indice=indice-1
+
+# Mostramos la matriz original
+print("Matriz original:")
+imprimir_matriz(matriz)
+
+# Ordenamos la segunda fila (índice)
+ordenar_fila(matriz, indice)
+
+# Mostramos la matriz después de ordenar la segunda fila
+print("\nMatriz con la segunda fila ordenada:")
+imprimir_matriz(matriz)
