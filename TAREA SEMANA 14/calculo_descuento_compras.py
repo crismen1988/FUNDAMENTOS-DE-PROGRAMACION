1b381128e4c1979aa6fd2adfe00f8b45d4a42a40
Cristian Meneses <25crismen@gmail.com>
2024-09-21 18:06:29 -0500

    VERSION 1

--- TAREA SEMANA 14/calculo_descuento_compras.py
+++ TAREA SEMANA 14/calculo_descuento_compras.py
@@ -11,13 +11,14 @@ def escoger_productos(electro,precio_electro,codigo_electro):
         print(f" {codigo_electro[i]}                       {electro[i]}                    {precio_electro[i]}")
   
      
-def compra(respuesta_usuario,codigo_electro,electro,precio_producto):
+def compra(codigo_electro,electro,precio_producto):
     
     print("\n\033[39m"+"")
     global total_compra
+    global codigo_erroneo
     total_compra=0
-    while respuesta_usuario=="s":
-        codigo_electro_usuario=input("\nIngrese el código del producto: ").lower()
+    while True:
+        codigo_electro_usuario=input("\n\033[;39m"+"Ingrese el código del producto o 'S' para salir: ").lower()
         if codigo_electro_usuario in codigo_electro:
             indice_codigo=codigo_electro.index(codigo_electro_usuario)
             numero_produc=int(input("\nIngrese el número de Artefectos: "))
@@ -26,11 +27,13 @@ def compra(respuesta_usuario,codigo_electro,electro,precio_producto):
             print(f"Valor Individual: {precio_producto[indice_codigo]} $")
             print(f"Valor Total: {precio_total:.2f} $")
             total_compra+=(precio_total)
+            codigo_erroneo=False
+        elif codigo_electro_usuario=="s":
+            break
         else:
-            print("\nIngreso Un Código de Producto Erróneo")
-        respuesta_usuario=input("\n\n\033[39m"+"Desea Agregar Otro Producto a la Compra S/N: ").lower()
-    print("\033[;36m"+"")
-    print(f"\nEl Total De La Venta Es: {total_compra:.2f} $")
+            codigo_erroneo=True
+            print("\n\033[;91m"+"***** Ingreso Un Código de Producto Erróneo *****")
+       
     
 
 def descuento(compra_total,descuento_personalizado=None):
@@ -46,7 +49,7 @@ def descuento(compra_total,descuento_personalizado=None):
             total_pagar=compra_total-descuento
             print("\033[;36m"+"")
             print(f"\nTotal a Pagar con descuento: {total_pagar:.2f} $")
-        else:
+        elif compra_total>500:
             descuento=compra_total*0.1
             print(f"\nLa venta tiene un descuento del 10 %: {descuento:.2f} $")
             total_pagar=compra_total-descuento
@@ -58,23 +61,33 @@ def descuento(compra_total,descuento_personalizado=None):
         print(f"\nLa venta tiene un descuento del {descuento_personalizado} %: {descuento:.2f} $")
         total_pagar=compra_total-descuento
         print("\033[;36m"+"")
-        print(f"\nTotal a Pagar con descuento del {descuento_personalizado} %: {total_pagar:.2f} $")
+        print(f"\nTotal a Pagar con descuento del {descuento_personalizado} %: {total_pagar:.2f} $\n")
 
         
-respuesta_usuario="s"
-while respuesta_usuario=="s":
+
+while True:
     escoger_productos(electrodomesticos,precios_electrodomesticos,codigo_electrodomestico)
-    compra(respuesta_usuario,codigo_electrodomestico,electrodomesticos,precios_electrodomesticos)
+    compra(codigo_electrodomestico,electrodomesticos,precios_electrodomesticos)
     descuento(total_compra)
-    respuesta_descuento=input("\n\033[1;33m"+"¿Desea dar algún descuento personalizado S/N ?: ").lower()
-    if respuesta_descuento=="s":
-        descuento_personalizado=input("\n\033[39m"+"\nIngrese el valor del descuento personalizado: ")
-        descuento(total_compra,descuento_personalizado)
-    else:
-        print("\n\033[39m"+"\nNo ha dado ningún descuento")
-    print("\n\033[39m"+"")
-    respuesta_usuario=input("\nPresione 'S' para generar una nueva venta o Cualquier tecla para salir del sistema de ventas: ").lower    
-print("\nHasta Pronto..........")
+    if codigo_erroneo==True:
+        respuesta_usuario=input("\n\033[92m"+"Presione 'N' para generar una nueva venta o Cualquier tecla para salir del sistema de ventas: ").lower()    
+        if respuesta_usuario!="n":
+            print("\n\033[39m"+"")
+            print("\nHasta Pronto............")
+            break
+    elif codigo_erroneo==False:
+        respuesta_descuento=input("\n\033[1;33m"+"¿Desea dar algún descuento personalizado S/N ?: ").lower()
+        if respuesta_descuento=="s":
+            descuento_personalizado=input("\n\033[39m"+"\nIngrese el valor del descuento personalizado: ")
+            descuento(total_compra,descuento_personalizado)
+        else:
+            print("\n\033[39m"+"No ha dado ningún descuento\n")
+            respuesta_usuario=input("\n\033[92m"+"Presione 'N' para generar una nueva venta o Cualquier tecla para salir del sistema de ventas: ").lower()    
+            if respuesta_usuario!="n":
+                print("\n\033[39m"+"")
+                print("\nHasta Pronto............")
+                break
+        
 
         
         
